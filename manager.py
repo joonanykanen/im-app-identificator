d7a1f8cf986f8c3a46504ab16937fba30699dad8
Joona Nykänen <joona.nykanen@student.lut.fi>
2023-01-28 20:02:49 +0200

    feat: Added manager

---
+++ manager.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# Made by: Joona Nykänen
+# Date: 28.01.2023
+
+from Identificator import Identificator
+
+if __name__ == '__main__':
+    sampleURI = "visma-identity://login?source=severa"
+    identity = Identificator(sampleURI)
+
+    # None in case of an invalid path
+    if identity.path != None:
+        print(f"Path: {identity.path}")
+
+    # None in case of invalid parameters
+    if identity.params != None:
+        print(f"Parameters: {identity.params}")
+
+    if identity.path == None:
+        print(f"Invalid URI. Please try again.")
+    elif identity.params == None:
+        print(f"Invalid {identity.path}-parameters. Please try again.")
+
+# eof
