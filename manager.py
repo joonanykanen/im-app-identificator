371d9d7a50983969f108a4e036bbafd478dd0502
Joona Nykänen <joona.nykanen@student.lut.fi>
2023-01-29 13:56:17 +0200

    feat: added Client

--- manager.py
+++ manager.py
@@ -5,21 +5,28 @@
 
 from Identificator import Identificator
 
+
+class Client:
+    def __init__(self, uri: str):
+        identity = Identificator(sampleURI)
+
+        # None if path is invalid
+        if identity.path != None:
+            print(f"Path: {identity.path}")
+
+        # None if parameters are invalid
+        if identity.params != None:
+            print(f"Parameters: {identity.params}")
+
+        if identity.path == None:
+            print(f"Invalid URI. Please try again.")
+        elif identity.params == None:
+            print(f"Invalid {identity.path}-parameters. Please try again.")
+
+
 if __name__ == '__main__':
     sampleURI = "visma-identity://confirm?source=netvisor&paymentnumber=102226"
-    identity = Identificator(sampleURI)
+    client = Client(sampleURI)
 
-    # None in case of an invalid path
-    if identity.path != None:
-        print(f"Path: {identity.path}")
-
-    # None in case of invalid parameters
-    if identity.params != None:
-        print(f"Parameters: {identity.params}")
-
-    if identity.path == None:
-        print(f"Invalid URI. Please try again.")
-    elif identity.params == None:
-        print(f"Invalid {identity.path}-parameters. Please try again.")
 
 # eof
